21eda5a722386bf5cb92aa5508d6eae4b74291a6
Lee Sangdae <mousesd@gmail.com>
2018-04-18 17:28:06 +0900

    Reusable EffectBehavior

---
+++ FormsApp24/FormsApp24/FormsApp24.Android/Effects/LabelShadowEffect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Android.Util;
+using Android.Widget;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+using FormsApp24.Droid.Effects;
+
+[assembly:ResolutionGroupName("Xamarin")]
+[assembly:ExportEffect(typeof(LabelShadowEffect), "LabelShadowEffect")]
+namespace FormsApp24.Droid.Effects
+{
+    public class LabelShadowEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            if (!(this.Control is TextView textView))
+                return;
+
+            textView.SetShadowLayer(5, 5, 5, Color.Black.ToAndroid());
+        }
+
+        protected override void OnDetached()
+        {
+            
+        }
+    }
+}
---
+++ FormsApp24/FormsApp24/FormsApp24.iOS/Effects/LabelShadowEffect.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using CoreGraphics;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+using FormsApp24.iOS.Effects;
+
+[assembly: ResolutionGroupName("Xamarin")]
+[assembly: ExportEffect(typeof(LabelShadowEffect), "LabelShadowEffect")]
+namespace FormsApp24.iOS.Effects
+{
+    public class LabelShadowEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            this.Control.Layer.CornerRadius = 5;
+            this.Control.Layer.ShadowColor = Color.Black.ToCGColor();
+            this.Control.Layer.ShadowOffset = new CGSize(5, 5);
+            this.Control.Layer.ShadowOpacity = 1;
+        }
+
+        protected override void OnDetached()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
